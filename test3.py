6a60e53cb7ab16fa64c846548dcccd55cd3aec5b
ivan002323 <ivancantero023@gmail.com>
2021-10-05 02:42:27 +0000

    prelim_exam

--- test3.py
+++ test3.py
@@ -5,8 +5,12 @@ with open('covid_cases.json', 'r') as json_file:
     ourjson = json.load(json_file)
 covid_data =ourjson['records']
 data_file = open('data.csv', 'w')
-csv.writer = csv.writer(data_file)
+csv_writer = csv.writer(data_file)
 count = 0
 for i in covid_data:
     if count == 0:
         header = i.keys()
+        csv_writer.writerow(header)
+        count +=1
+    csv_writer.writerow(i.values())
+data_file.close()
